764256f0bf8479e8ea4b18e0b4c889acde193fff
Joanna Waczyńska <joanna.waczynska@orange.com>
2023-08-03 16:08:54 +0200

    init fork

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="nerf_pytorch",
-    version="0.0.1",
-    description="Nerf",
-    author="waczyjoan",
-    packages=find_packages(),
-)
